1ebce564be97ac70f7763237b1ff7935349f1b20
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2014-06-24 01:38:18 +0400

    pelicanized

---
+++ src/pelicanconf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'\u041f\u0430\u0432\u0435\u043b \u0421\u0443\u0442\u044b\u0440\u0438\u043d'
+SITENAME = u'\u041f\u0430\u0432\u0435\u043b \u0421\u0443\u0442\u044b\u0440\u0438\u043d'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'ru'
+DEFAULT_DATE = 'fs'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+DISPLAY_PAGES_ON_MENU = True
+
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
